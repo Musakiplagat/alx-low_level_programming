5f4c7c67b3d741adecd803a02d37734a8bbab741
Musakiplagat <musakiplagat@gmail.com>
2022-09-15 05:02:16 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main . print the string in the put fucntion
+*
+*description:ufing the main function
+*this progtam prints "programming is like building a multilingual puzzle
+*retuen: 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+
