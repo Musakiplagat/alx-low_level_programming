f72f5e90e33906ae904bbf709db13440f5740898
Musakiplagat <musakiplagat@gmail.com>
2022-09-17 03:25:39 -0700

    commitu

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
 *this program prints "Programming is like building a multilingual puzzle"
 *returns :0
 */
-int main(void)
+int main(void);
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
