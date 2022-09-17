e3dd8c2752766df922b6bb3d34bc510658d69e29
Musakiplagat <musakiplagat@gmail.com>
2022-09-17 03:11:35 -0700

    4commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+/**
+*main-print the string in the put function
+*
+*Description:uding the main fuction
+*this program prints "programming is like building a multilingual puzzle
+*Return :0
+*/
+int main(void)
+{
+	CHAR C
+	int i;
+long long lli;
+	float f;
