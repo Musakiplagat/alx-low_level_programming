df2fee607cda81fdc3d0dc37c107279216d9deef
Musakiplagat <musakiplagat@gmail.com>
2022-09-17 03:38:09 -0700

    coooomd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@ aiinclude <ng is like building a multilingual puzzle"atdio.h>
 *main - print the string in the function
 *
 *description:using the main function
-*this program prints "Programming is like building a multilingual puzzle"
+*this program prints ""Programming is like building a multilingual puzzle"
 *returns :0
 */
 int main(void)
